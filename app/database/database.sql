8fd55e13eadc86d2ce7fd6b5da43dac181bfa0e6
anymore07 <meylakusuma07@gmail.com>
2023-10-24 09:08:17 +0700

    update database

--- app/database/database.sql
+++ app/database/database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 09 Okt 2023 pada 03.21
--- Versi server: 8.0.30
--- Versi PHP: 8.1.10
+-- Generation Time: Oct 24, 2023 at 02:05 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,18 +18,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `perpustakaan`
+-- Database: `sposerp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `table_template`
+-- Table structure for table `laporan_pengeluaran`
 --
 
-CREATE TABLE `table_template` (
+CREATE TABLE `laporan_pengeluaran` (
   `id` int NOT NULL,
   `uuid` char(36) NOT NULL,
+  `menu` text NOT NULL,
+  `tanggal` date NOT NULL,
+  `quantity` int NOT NULL,
   `note` varchar(50) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `created_by` char(36) NOT NULL,
@@ -47,7 +50,108 @@ CREATE TABLE `table_template` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `shipment`
+--
+
+CREATE TABLE `shipment` (
+  `id` int NOT NULL,
+  `uuid` char(36) NOT NULL,
+  `harga` int NOT NULL,
+  `unit_harga` varchar(20) NOT NULL,
+  `menu` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pesan` int NOT NULL,
+  `unit_pesan` varchar(20) NOT NULL,
+  `berat` int NOT NULL,
+  `unit_berat` varchar(20) NOT NULL,
+  `harga_exw` int NOT NULL,
+  `total_exw` int NOT NULL,
+  `ongkir` int NOT NULL,
+  `ice_pack` int NOT NULL,
+  `diskon` int NOT NULL,
+  `total` int NOT NULL,
+  `keterangan` text NOT NULL,
+  `note` varchar(50) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` char(36) NOT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` char(36) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) NOT NULL,
+  `restored_at` datetime DEFAULT NULL,
+  `restored_by` char(36) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `is_restored` tinyint(1) NOT NULL,
+  `status` tinyint(1) GENERATED ALWAYS AS ((case when ((`is_deleted` = 0) and (`is_restored` = 0)) then _utf8mb4'1' when ((`is_deleted` = 1) and (`is_restored` = 0)) then _utf8mb4'0' when ((`is_deleted` = 0) and (`is_restored` = 1)) then _utf8mb4'1' end)) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_bahan`
+--
+
+CREATE TABLE `stok_bahan` (
+  `id` int NOT NULL,
+  `uuid` char(36) NOT NULL,
+  `menu` text NOT NULL,
+  `tanggal` date NOT NULL,
+  `masuk` int NOT NULL,
+  `stok` int NOT NULL,
+  `keluar` int NOT NULL,
+  `note` varchar(50) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` char(36) NOT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` char(36) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) NOT NULL,
+  `restored_at` datetime DEFAULT NULL,
+  `restored_by` char(36) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `is_restored` tinyint(1) NOT NULL,
+  `status` tinyint(1) GENERATED ALWAYS AS ((case when ((`is_deleted` = 0) and (`is_restored` = 0)) then _utf8mb4'1' when ((`is_deleted` = 1) and (`is_restored` = 0)) then _utf8mb4'0' when ((`is_deleted` = 0) and (`is_restored` = 1)) then _utf8mb4'1' end)) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int NOT NULL,
+  `uuid` varchar(36) NOT NULL,
+  `nama` text NOT NULL,
+  `alamat` text NOT NULL,
+  `kontak` text NOT NULL,
+  `email` text NOT NULL,
+  `note` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` char(36) NOT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` char(36) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) NOT NULL,
+  `restored_at` datetime DEFAULT NULL,
+  `restored_by` char(36) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `is_restored` tinyint(1) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `uuid`, `nama`, `alamat`, `kontak`, `email`, `note`, `created_at`, `created_by`, `modified_at`, `modified_by`, `deleted_at`, `deleted_by`, `restored_at`, `restored_by`, `is_deleted`, `is_restored`, `status`) VALUES
+(1, '36929f29-b36f-4e8d-9012-3cd006c85c33', 'Daging enak', 'mana aja', '08123382520', 'teampapathore@gmail.com', '', '2023-10-23 18:54:11', 'admin', NULL, '', '2023-10-23 19:03:34', 'admin', NULL, '', 1, 0, 0),
+(2, '48bb7345-1699-4abc-b670-0121341c47cb', 'Daging enak', 'mana aja', '08123382520', 'email@example.com', '', '2023-10-23 19:04:04', 'admin', NULL, '', NULL, '', NULL, '', 0, 0, 1),
+(3, '760633c2-1d7d-46cf-aa96-8b6e29760e3e', 'mang ea?', 'bumi', '089520409050', 'mellandakumalasari13@gmail.com', '', '2023-10-23 20:07:07', 'admin', '2023-10-23 23:04:06', 'admin', NULL, '', NULL, '', 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -61,7 +165,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `last_login_at`, `status`) VALUES
@@ -72,29 +176,65 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `last_login_
 --
 
 --
--- Indeks untuk tabel `table_template`
+-- Indexes for table `laporan_pengeluaran`
 --
-ALTER TABLE `table_template`
+ALTER TABLE `laporan_pengeluaran`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `shipment`
+--
+ALTER TABLE `shipment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stok_bahan`
+--
+ALTER TABLE `stok_bahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `table_template`
+-- AUTO_INCREMENT for table `laporan_pengeluaran`
 --
-ALTER TABLE `table_template`
+ALTER TABLE `laporan_pengeluaran`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `shipment`
+--
+ALTER TABLE `shipment`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stok_bahan`
+--
+ALTER TABLE `stok_bahan`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
