432f33482298142c0d9884cab977fd5ff60a2bc7
benita alexandra <124318206+menayangbenita@users.noreply.github.com>
2023-10-26 14:47:02 +0700

    update icon

--- app/controllers/Kasir.php
+++ app/controllers/Kasir.php
@@ -11,6 +11,8 @@ class Kasir extends Controller
 		$data['title'] = 'Menu';
 		$data['user'] = $this->user;
         $data['menu'] = $this->model('Menu_model')->getAllData();
+        // $data['stok'] = $this->model('kelolaStok_model')->getAllData();
+
 		
 		$this->view('kasir', $data);
 	}
--- app/views/kasir.php
+++ app/views/kasir.php
@@ -1,5 +1,12 @@
 <?php if ($data['user']): ?>
     <?php require_once "templates/header.php" ?>
+    <style>
+        .card-img-top {
+            object-fit: cover;
+            height: 150px;
+            width: 100%;
+        }
+    </style>
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <div class="row">
         <!-- <div class="col-12">
@@ -70,51 +77,40 @@
         </div>
 
         <div class="col-lg-6">
+            <h5 class="mb-4 align-bottom">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                    <style>svg{fill:#3a4168}</style>
+                    <path d="M0 192c0-35.3 28.7-64 64-64c.5 0 1.1 0 1.6 0C73 91.5 105.3 64 144 64c15 0 29 4.1 40.9 11.2C198.2 49.6 225.1 32 256 32s57.8 17.6 71.1 43.2C339 68.1 353 64 368 64c38.7 0 71 27.5 78.4 64c.5 0 1.1 0 1.6 0c35.3 0 64 28.7 64 64c0 11.7-3.1 22.6-8.6 32H8.6C3.1 214.6 0 203.7 0 192zm0 91.4C0 268.3 12.3 256 27.4 256H484.6c15.1 0 27.4 12.3 27.4 27.4c0 70.5-44.4 130.7-106.7 154.1L403.5 452c-2 16-15.6 28-31.8 28H140.2c-16.1 0-29.8-12-31.8-28l-1.8-14.4C44.4 414.1 0 353.9 0 283.4z"/>
+                </svg>
+                Makanan
+                </h5>
             <div class="row">
-                <div class="col-lg-4 mb-5">
+                
+                <?php foreach ($data["menu"] as $menu): ?>
+                    <?php $harga = number_format($menu['harga'], 0, '.', '.'); ?>
+                <div class="col-lg-4 mb-4">
                     <a href="#">
-                        <button class="card custom-card addProductSale p-0" style="width: 100%">
-                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="Gambar Produk">
-                            <div class="card-body">
-                                <h7 class="card-title">Sayur</h7>
-                                <p class="text-secondary text-start m-0">20</p>
-                            </div>
+                        <button class="card h-100 animation-card addProductSale p-0" style="width: 100%">
+                            <img src="<?= BASEURL ?>/img/datafoto/<?= $menu['foto'] ?>" alt="img <?= $menu['foto'] ?>" class="card-img-top">
+                            <div class="card-body text-start pb-0">
+                                        <h7 class="card-title">
+                                            <?= $menu['nama']; ?>
+                                        </h7>
+                                        <h6 class="card-subtitle mt-2 mb-3 text-md text-muted">Rp
+                                            <?= $harga; ?>
+                                        </h6>
+                                        <!-- <div class="border-top py-2"></div>
+                                        <h6 class="mb-0 text-muted text-sm">
+                                            <?= $menu['bahan']; ?>
+                                        </h6>
+                                        <h6 class="mb-3 text-muted text-sm">
+                                            <?= $menu['jumlah']; ?>
+                                        </h6> -->
+                                    </div>
                         </button>
                     </a>
                 </div>
-                <div class="col-lg-4 mb-5">
-                    <a href="#">
-                        <button class="card custom-card addProductSale p-0" style="width: 100%">
-                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="Gambar Produk">
-                            <div class="card-body">
-                                <h7 class="card-title">Daging</h7>
-                                <p class="text-secondary text-start m-0">20</p>
-                            </div>
-                        </button>
-                    </a>
-                </div>
-                <div class="col-lg-4 mb-5">
-                    <a href="#">
-                        <button class="card custom-card addProductSale p-0" style="width: 100%">
-                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="Gambar Produk">
-                            <div class="card-body">
-                                <h7 class="card-title">Buah</h7>
-                                <p class="text-secondary text-start m-0">20</p>
-                            </div>
-                        </button>
-                    </a>
-                </div>
-                <div class="col-lg-4 mb-5">
-                    <a href="#">
-                        <button class="card custom-card addProductSale p-0" style="width: 100%">
-                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="Gambar Produk">
-                            <div class="card-body">
-                                <h7 class="card-title">Steak</h7>
-                                <p class="text-secondary text-start m-0">20</p>
-                            </div>
-                        </button>
-                    </a>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
 
@@ -126,7 +122,7 @@
                             <span class="input-group-text" id="basic-addon1">
                                 <i class="fa fa-user ps-2"></i>
                             </span>
-                            <input type="text" class="form-control ps-2" value="admin" readonly>
+                            <input type="text" class="form-control ps-2" value="<?= $data['user']['username'] ?>" readonly>
                         </div>
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">
